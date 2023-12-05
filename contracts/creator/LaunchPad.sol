75e6e253c51fb41fae791a46c051cf1183f6542f
quyphandang <dangquyphan@gmail.com>
2023-12-05 18:20:36 +0700

    remove flatten file

--- contracts/creator/LaunchPad.sol
+++ contracts/creator/LaunchPad.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 import { ERC1155Holder } from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
